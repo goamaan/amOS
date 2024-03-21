f045ca1f15173385ec335ffd939fbcfcaa397304
goamaan <amaangokak18@gmail.com>
2024-03-21 16:32:01 -0700

    fix postview max-h

--- src/components/Post/PostView.tsx
+++ src/components/Post/PostView.tsx
@@ -3,7 +3,7 @@ import { Editor } from "~/components/editor/editor"
 
 export function PostView({ post }: { post: Post }) {
   return (
-    <div className="flex max-h-screen flex-col space-y-4">
+    <div className="flex flex-col space-y-4">
       <h1 className="text-2xl font-semibold">{post.title}</h1>
       <Editor content={post.content} />
     </div>
